8dc4d02df34d0489874728cb77d476b9af0f4345
Mike Pretzlaw <pretzlaw@gmail.com>
2014-03-14 14:10:57 +0000

    A simple view or block

---
+++ View.php
@@ -0,0 +1,66 @@
+<?php
+
+class View
+{
+    protected $_file;
+    protected $_data;
+
+    public function __construct($file)
+    {
+        if (!stream_resolve_include_path($file))
+        {
+            throw new InvalidArgumentException(
+                sprintf('File "%s" was not found.', $file)
+            );
+        }
+
+        $this->_file = $file;
+    }
+
+    public function setData($key, $value)
+    {
+        $this->_data[$key] = $value;
+    }
+
+    public function getData($key)
+    {
+        if (!isset($this->_data[$key]))
+        {
+            return null;
+        }
+
+        return $this->_data[$key];
+    }
+
+    public function __invoke()
+    {
+        $this->dispatch();
+    }
+
+    public function dispatch()
+    {
+        echo $this->render();
+    }
+
+    public function render()
+    {
+        ob_start();
+        extract($this->_data, EXTR_OVERWRITE);
+        include $this->getFile();
+
+        return ob_get_clean();
+    }
+
+    public function __toString()
+    {
+        return $this->render();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFile()
+    {
+        return $this->_file;
+    }
+}
